82c9252481ad0adf0b5f2c9b33a386415f8d6d21
irfn <mail.urfi@gmail.com>
2009-07-12 18:32:48 +0530

    Initial configuration for cedit and ecb

---
+++ packages/ecb-init.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/.emacs.d/packages/cedit")
+(add-to-list 'load-path "~/.emacs.d/packages/ecb")
+(load-file "~/.emacs.d/packages/cedit/common/cedet.el")
+(load-file "~/.emacs.d/packages/ecb/ecb.el")
+(require 'ecb)
---
+++ packages/inkpot-init.el
@@ -0,0 +1,4 @@
+(require 'color-theme)
+(color-theme-initialize)
+(load-file "~/.emacs.d/packages/inkpot-mod/inkpot.el")
+(color-theme-inkpot)
