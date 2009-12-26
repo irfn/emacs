7134ffabb5435202bff1f1199123f83b539f6441
irfn <mail.urfi@gmail.com>
2009-12-26 18:47:23 +0530

    adding espresso mode

---
+++ packages/espresso-init.el
@@ -0,0 +1,4 @@
+(setq load-path (cons  "~/.emacs.d/packages/espresso.el" load-path))
+(autoload #'espresso-mode "espresso" "Start espresso-mode" t)
+(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
