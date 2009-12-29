51223ea63dff2440d8f42a1c297a5a7e29089586
irfn <mail.urfi@gmail.com>
2009-12-29 16:06:08 +0530

    added nxhtml and customize faces for it

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
 (load-file "~/.emacs.d/packages/inkpot-init.el")
 (load-file "~/.emacs.d/packages/zen-init.el")
 (load-file "~/.emacs.d/packages/highlight-current-line.el")
-
+(load "~/.emacs.d/packages/nxhtml/autostart.el")
 ;;(load-file "~/.emacs.d/packages/twilight-init.el")
 ;;(load-file "~/.emacs.d/packages/zenburn-init")
 
@@ -42,3 +42,31 @@
 (org-mode)
 (color-theme-zen-and-art)
 (put 'dired-find-alternate-file 'disabled nil)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(case-fold-search nil)
+ '(cua-mode nil nil (cua-base))
+ '(ecb-layout-name "left3")
+ '(ecb-layout-window-sizes (quote (("left3" (0.1935483870967742 . 0.43137254901960786) (0.1935483870967742 . 0.3333333333333333) (0.1935483870967742 . 0.21568627450980393)))))
+ '(ecb-options-version "2.33beta2")
+ '(ecb-source-path my-global-projects-dirs)
+ '(ecb-tip-of-the-day nil)
+ '(linum-format "%4d ")
+ '(mumamo-chunk-coloring 0)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(transient-mark-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "#2A2828"))))
+ '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "#373436")))))
+
+(ecb-toggle-ecb-windows)
--- packages/ecb-init.el
+++ packages/ecb-init.el
@@ -4,23 +4,4 @@
 (load-file "~/.emacs.d/packages/ecb/ecb.el")
 (require 'ecb)
 
-;; Show the source file under the directories
-;;(setq ecb-show-sources-in-directories-buffer '("left7" "left13" "left14" "left15"))
-
-(custom-set-variables
- '(ecb-layout-window-sizes (quote (("left3" (0.1935483870967742 . 0.43137254901960786) (0.1935483870967742 . 0.3333333333333333) (0.1935483870967742 . 0.21568627450980393)))))
- '(ecb-options-version "2.33beta2")
- '(ecb-source-path my-global-projects-dirs)
- '(ecb-tip-of-the-day nil)
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(case-fold-search nil)
- '(cua-mode nil nil (cua-base))
- '(transient-mark-mode t)
- '(show-paren-mode t))
-
-(ecb-layout-switch "left3")
 (ecb-activate)
-
-(ecb-activate)
-(ecb-toggle-ecb-windows)
