79ece0e954cc186c18ed560c804dbcca42344128
irfn <mail.urfi@gmail.com>
2010-01-23 00:14:35 +0530

    a sample growl- on a success/failed compilation mode finish => an rspec run

--- packages/growl-init.el
+++ packages/growl-init.el
@@ -1,2 +1,9 @@
 (setq load-path (cons  "~/.emacs.d/packages/growl" load-path))
 (require 'growl)
+;; need a smarter check than finished. like differentiate between rpec / other compilation modes
+(defun growl-a-bit(buffer string)
+	(cond ((string-match "finished" string)
+		(growl "yoo" "hoo"))
+		(0 (growl "some" "insult"))))
+
+(add-hook 'compilation-finish-functions 'growl-a-bit)
