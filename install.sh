24431aada08fc670aa1058c1ba52db5df7d9d79e
irfn <mail.urfi@gmail.com>
2010-01-23 22:08:50 +0530

    more generic install script

--- install.sh
+++ install.sh
@@ -6,23 +6,48 @@ function echoWithBars() {
 	echobar	
 }
 
-git submodule init
-echoWithBars "submodules inited"
-git submodule update
-echoWithBars "submodules updated"
-
-
-# make-cedit : 
-make -s -C packages/cedit
-echoWithBars "cedit completed"
-
-# make-ecb : 
+# git submodule init
+# echoWithBars "submodules inited"
+# git submodule update
+# echoWithBars "submodules updated"
+# 
+# 
 sed -e 's/^CEDET.*$/CEDET=\~\/\.emacs\.d\/packages\/cedit/' packages/ecb/Makefile > packages/ecb/MakefileMod
-make -s -C packages/ecb -f MakefileMod
 
 
-echoWithBars "ecb completed"
+function emacsMake() {
+	if [ `ls $1 | grep MakefileMod | wc -l` -ne 0 ]
+	then
+		make -s -C $1 -f $1/MakefileMod
+	elif [ `ls $1 | grep Makefile | wc -l` -ne 0 ]
+	then
+		make -s -C $1
+	else
+		emacs -batch -f batch-byte-compile $1/*.el
+	fi		
+	echoWithBars  $1 + "completed "
+}
 
-make -s -C packages/distel
+function emacsMakeProjects() {
+	echo `git submodule | awk '{print $2}'`
+}
 
-echoWithBars "distel completed"
+function headOf() {
+	echo $1 | awk -F" " '{print $1}'
+}
+
+function tailOf() {
+	echo $1 | awk -F" " '{print $1}'
+}
+
+function makeProjects() {	
+	projects=`emacsMakeProjects`
+	while [ `echo $projects| wc -w` -ne 0 ]
+	do
+		project=`headOf $projects`
+		emacsMake $project
+		projects=`echo $projects | sed "s,$project,,"`
+	done
+}  
+
+makeProjects
