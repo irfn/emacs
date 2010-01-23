e984360f5a633c647640563c1e6d9a59306cc881
irfn <mail.urfi@gmail.com>
2010-01-23 16:01:25 +0530

    adding an install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+lias echobar="echo \"=====================================================================================================================\""
+
+function echoWithBars() {
+	echobar
+	echo  $1
+	echobar	
+}
+
+git submodule init
+echoWithBars "submodules inited"
+git submodule update
+echoWithBars "submodules updated"
+
+
+# make-cedit : 
+make -s -C packages/cedit
+echoWithBars "cedit completed"
+
+# make-ecb : 
+sed -e 's/^CEDET.*$/CEDET=\~\/\.emacs\.d\/packages\/cedit/' packages/ecb/Makefile > packages/ecb/MakefileMod
+make -s -C packages/ecb -f MakefileMod
+
+
+echoWithBars "ecb completed"
+
+make -s -C packages/distel
+
+echoWithBars "distel completed"
