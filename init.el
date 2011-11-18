3b5305d2caea457f492a4a878d87c50b473f61d2
Irfan Shah <mail.urfi@gmail.com>
2011-11-18 12:07:46 +0530

    move to elpa

--- init.el
+++ init.el
@@ -1,1 +1,19 @@
-;; 
+(add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/themes/emacs-color-theme-solarized/")
+(setq custom-file "~/.emacs.d/custom.el")
+
+
+(require 'marmalade)
+(require 'solarized-dark)
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("config.ru" . ruby-mode))
+(setq *textmate-gf-exclude*
+  "/\\.|vendor|fixtures|tmp|log|build|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc|\\.elc|\\.jar|\\.class")
+(setq rinari-tags-file-name "TAGS")
+
+(load-file "~/.emacs.d/custom.el")
+(linum-mode)
+(highline-mode)
+(set-face-background 'highline-face "#222")
