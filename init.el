a25298773c9a901d95d99b465208ac684eecc773
Irfan Shah <mail.urfi@gmail.com>
2012-02-06 10:01:35 +0530

    more modes

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 (require 'campfire)
 (require 'rvm)
 (require 'rspec-mode)
-
+(require 'gtags)
 
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
