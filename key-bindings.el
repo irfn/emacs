4c94d1bef6cff3642931317d6795ddbe052d5d59
Irfan Shah <mail.urfi@gmail.com>
2012-02-11 11:15:52 +0530

    added keybindings and dup line

---
+++ key-bindings.el
@@ -0,0 +1,23 @@
+(global-set-key (kbd "A-d") "\C-a\C- \C-n\M-w\C-y")
+(global-set-key (kbd "A-D") "\M-w\C-y")
+
+(global-set-key "\C-cc" 'mode-compile)
+(global-set-key "\C-ck" 'mode-compile-kill)
+
+(global-set-key (kbd "\C-cd") 'duplicate-line)
+
+(define-key ac-completing-map "\M-n" 'ac-next)
+(define-key ac-completing-map "\M-p" 'ac-previous)
+(define-key ac-mode-map (kbd "\M-/") 'auto-complete) 
+(define-key ac-mode-map (kbd "ESC M-/") 'ac-complete-dictionary)
+(define-key ac-mode-map (kbd "ESC M-o") 'dired)
+(define-key ac-mode-map (kbd "ESC M-j") 'ac-complete-yasnippet)
+(global-set-key "\M-/" 'auto-complete)
+(global-set-key "\M-?" 'dabbrev-expand)
+(global-set-key (kbd "\C-co") 'open-buffer-path)
+
+(global-set-key "\C-cy" 'browse-kill-ring)
+
+(global-set-key (kbd "ESC M-a") 'ack)
+
+(provide 'key-bindings)
