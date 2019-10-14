8668ecde9c5040de157b5425af9c26c01d0dc4cd
irfan <irfan@go-jek.com>
2019-10-14 07:13:46 +0530

    change theme

--- custom.el
+++ custom.el
@@ -5,12 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "152c9642180cb0907bfe7c343ed07d0586c0d84fd8e7279d90566088989a13bb" "72559f34f255a17b7055687c1593ae1dbbd3c5b61b56d0ca96f9468ca228b96c" default)))
- '(package-selected-packages
-   (quote
-    (docker docker-compose-mode k8s-mode doom-modeline ox-gfm ob-sml sml-mode emacs-elixir memoize all-the-icons org-gcal dockerfile-mode intero haskell-mode flycheck-golangci-lint plantuml-mode org-bullets emoji-cheat-sheet-plus company-emoji ob-go ob terraform-mode go-guru org-present go-snippets alchemist racer cargo flymake-rust rust-mode go-eldoc flymake-go company-go go-mode use-package suscolors-theme magit helm-projectile)))
- '(with-editor-emacsclient-executable nil)
- '(with-editor-shell-command-use-emacsclient nil))
+    ("585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
