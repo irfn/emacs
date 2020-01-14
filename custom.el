3be21bc4d878482a6bcfa4694108c437edf3e080
irfan <irfans@protonmail.com>
2020-01-14 11:12:19 +0530

    switch to srcery, add ansible vault

--- custom.el
+++ custom.el
@@ -3,12 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
  '(package-selected-packages
    (quote
-    (rbenv rspec-mode inf-ruby rubocopfmt ruby-hash-syntax flymake-ruby rubocop enh-ruby-mode protobuf-mode distel erlang-mode company-distel distel-completion-lib erlang load-env-vars auto-org-md use-package terraform-mode suscolors-theme smex rg rainbow-delimiters racer plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go neotree lsp-ui kubernetes k8s-mode jsonnet-mode intero inkpot-theme indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-go flycheck-golangci-lint exec-path-from-shell emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji clojure-mode-extra-font-locking cider cargo ample-zen-theme all-the-icons-dired alchemist ace-window))))
+    (srcery-theme vscdark-theme use-package terraform-mode suscolors-theme smex ruby-hash-syntax rubocopfmt rubocop rspec-mode rg rbenv rainbow-delimiters racer protobuf-mode plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go night-owl-theme neotree lsp-ui load-env-vars kubernetes k8s-mode jsonnet-mode intero inkpot-theme inf-ruby indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-ruby flymake-go flycheck-golangci-lint exec-path-from-shell erlang enh-ruby-mode emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji company-distel clojure-mode-extra-font-locking cider cargo auto-org-md atom-one-dark-theme ansible-vault ample-zen-theme all-the-icons-dired alchemist ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
