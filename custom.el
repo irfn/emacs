73cc860a2d74e01fbfbbcde2bd1f8cc58f4b9623
irfan <irfans@protonmail.com>
2020-03-06 17:45:32 +0530

    Rust mode fixes, ox-odt foe odt export.

--- custom.el
+++ custom.el
@@ -3,9 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-archives
+   (quote
+    (("org" . "http://orgmode.org/elpa/")
+     ("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "https://melpa.org/packages/")
+     ("ox-odt" . "https://kjambunathan.github.io/elpa/"))))
  '(package-selected-packages
    (quote
-    (srcery-theme vscdark-theme use-package terraform-mode suscolors-theme smex ruby-hash-syntax rubocopfmt rubocop rspec-mode rg rbenv rainbow-delimiters racer protobuf-mode plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go night-owl-theme neotree lsp-ui load-env-vars kubernetes k8s-mode jsonnet-mode intero inkpot-theme inf-ruby indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-ruby flymake-go flycheck-golangci-lint exec-path-from-shell erlang enh-ruby-mode emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji company-distel clojure-mode-extra-font-locking cider cargo auto-org-md atom-one-dark-theme ansible-vault ample-zen-theme all-the-icons-dired alchemist ace-window)))
+    (flycheck-rust ox-odt mustache-mode mustache lua-mode srcery-theme vscdark-theme use-package terraform-mode suscolors-theme smex ruby-hash-syntax rubocopfmt rubocop rspec-mode rg rbenv rainbow-delimiters racer protobuf-mode plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go night-owl-theme neotree lsp-ui load-env-vars kubernetes k8s-mode jsonnet-mode intero inkpot-theme inf-ruby indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-ruby flymake-go flycheck-golangci-lint exec-path-from-shell erlang enh-ruby-mode emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji company-distel clojure-mode-extra-font-locking cider cargo auto-org-md atom-one-dark-theme ansible-vault ample-zen-theme all-the-icons-dired alchemist ace-window)))
  '(plantuml-default-exec-mode (quote jar))
  '(plantuml-jar-args (quote ("-charset" "UTF-8" "--illegal-access" "deny"))))
 (custom-set-faces
