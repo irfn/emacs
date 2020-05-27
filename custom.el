d9b933864c37042af0f9cd9ea6258c3e1133c1a6
irfan <irfans@protonmail.com>
2020-05-27 22:28:32 +0530

    better dash, tweaks to agenda

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files "~/.agenda_files")
  '(package-archives
    (quote
     (("org" . "http://orgmode.org/elpa/")
