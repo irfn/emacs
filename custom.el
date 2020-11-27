f0cd145dc0eb4f96956110f16393eeff062087e7
Irfan Shah <irfns@protonmail.com>
2020-11-27 10:48:35 +0530

    update config to handle emacs 27, add rust ob, disable org-gcal for now

--- custom.el
+++ custom.el
@@ -3,18 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#1C1B19" "#EF2F27" "#519F50" "#FED06E" "#2C78BF" "#E02C6D" "#0AAEB3" "#4E4E4E"])
+ '(custom-safe-themes
+   '("5a02bb308119dba4fe2e2d1a6af221075a7f932afdc73f39e6967a0560fadf0e" default))
  '(org-agenda-files "~/.agenda_files")
  '(package-archives
-   (quote
-    (("org" . "http://orgmode.org/elpa/")
+   '(("org" . "http://orgmode.org/elpa/")
      ("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
-     ("ox-odt" . "https://kjambunathan.github.io/elpa/"))))
+     ("ox-odt" . "https://kjambunathan.github.io/elpa/")))
  '(package-selected-packages
-   (quote
-    (gotest dashboard flycheck-rust ox-odt mustache-mode mustache lua-mode srcery-theme vscdark-theme use-package terraform-mode suscolors-theme smex ruby-hash-syntax rubocopfmt rubocop rspec-mode rg rbenv rainbow-delimiters racer protobuf-mode plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go night-owl-theme neotree lsp-ui load-env-vars kubernetes k8s-mode jsonnet-mode intero inkpot-theme inf-ruby indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-ruby flymake-go flycheck-golangci-lint exec-path-from-shell erlang enh-ruby-mode emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji company-distel clojure-mode-extra-font-locking cider cargo auto-org-md atom-one-dark-theme ansible-vault ample-zen-theme all-the-icons-dired alchemist ace-window)))
- '(plantuml-default-exec-mode (quote jar))
- '(plantuml-jar-args (quote ("-charset" "UTF-8" "--illegal-access" "deny"))))
+   '(ob-rust erlang-mode gotest dashboard flycheck-rust ox-odt mustache-mode mustache lua-mode srcery-theme vscdark-theme use-package terraform-mode suscolors-theme smex ruby-hash-syntax rubocopfmt rubocop rspec-mode rg rbenv rainbow-delimiters racer protobuf-mode plantuml-mode ox-gfm org-present org-gcal org-bullets ob-sml ob-go night-owl-theme neotree lsp-ui load-env-vars kubernetes k8s-mode jsonnet-mode intero inkpot-theme inf-ruby indent-tools helm-rg helm-projectile gruvbox-theme go-snippets go-guru go-eldoc flymake-yaml flymake-rust flymake-ruby flymake-go flycheck-golangci-lint exec-path-from-shell erlang enh-ruby-mode emoji-cheat-sheet-plus dracula-theme doom-modeline dockerfile-mode docker-compose-mode docker company-lsp company-go company-emoji company-distel clojure-mode-extra-font-locking cider cargo auto-org-md atom-one-dark-theme ansible-vault ample-zen-theme all-the-icons-dired alchemist ace-window))
+ '(plantuml-default-exec-mode 'jar)
+ '(plantuml-jar-args '("-charset" "UTF-8" "--illegal-access" "deny"))
+ '(shell-file-name "/usr/local/bin/fish"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
